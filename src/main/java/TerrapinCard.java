6a4782c2cfc37bbfa5f8e256cc83206ff99c6d05
ericKim262 <eskimo262@gmail.com>
2020-10-16 14:36:05 +0000

    Completed terrapin card

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -5,4 +5,56 @@ public class TerrapinCard {
   public TerrapinCard(double balance){
     this.balance = balance;
   }
+
+  public void payEconomical(){
+    if(balance >= 2.5){
+      balance -= 2.5;
+    }else if(balance < 2.5){
+      balance = balance;
+    }
+  }
+
+  public void payGourmet(){
+    if(balance >= 4.0){
+      balance -= 4.0;
+    }else if(balance < 4.0){
+      balance = balance;
+    }
+  }
+
+  public void loadMoney(double amount){
+    if(amount < 0){
+      balance = balance;
+    }else if(amount > 150){
+      balance += 150;
+    }else{
+      balance += amount;
+    }
+  }
+
+  public double getBalance(){
+    return this.balance;
+  }
+
+  public String toString(){
+    return "The card has " + this.balance + " dollars";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
